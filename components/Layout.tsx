1569f1c9affe86a033ecdcc6330a61db007d7680
syd660015-lab <syd660015@gmail.com>
2026-02-11 17:12:16 +0200

    Create Layout component with props interface
    Define Layout component with props for children, active view, and setter.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+  activeView: any;
+  setActiveView: any;
+}
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
 
